6a20c04bf0c1a1e573a151155277a2f42a5bf0b9
DummK <fpolski22@gmail.com>
2024-11-17 17:31:20 +0100

    Dodanie

---
+++ start/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace start
+
+    class Game
+    {
+        private String playerChoice;
+        private String computerChoice;
+
+        public void PlayerChoosing()
+        {
+            Console.WriteLine("Papier, kamień czy nożyce");
+            playerChoice = Console.ReadLine().ToLower();
+        }
+
+        public void StartGame()
+        {
+            Console.WriteLine("----Game----");
+            while (true)
+            {
+                PlayerChoosing();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
