6ff17171939659c04313c3ae6fa746b0b4ab38eb
DummK <fpolski22@gmail.com>
2024-11-17 18:27:28 +0100

    release

--- start/obj/Release/net8.0/start.AssemblyInfo.cs
+++ start/obj/Release/net8.0/start.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("start")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0744566af7a5a5a7868df9f22e801902909ee5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("start")]
 [assembly: System.Reflection.AssemblyTitleAttribute("start")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
